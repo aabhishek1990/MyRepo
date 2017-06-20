08692a370630ce0a3754b614ca02ffe9df0c0210
Abhishek <https://github.com/aabhishek1990/my-work>
2017-06-20 17:35:24 +0530

    3 changes

--- src/com/nt/model/User.java
+++ src/com/nt/model/User.java
@@ -2,17 +2,16 @@ package com.nt.model;
 
 public class User {
 
-	private int    id;
+	private int    id;	
+	private String name;
+	private String address;
+	
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
 	}
-	private String name;
-	private String address;
-	private String mobile;
-	
 	
 	public String getName() {
 		return name;
@@ -26,11 +25,6 @@ public class User {
 	public void setAddress(String address) {
 		this.address = address;
 	}
-	public String getMobile() {
-		return mobile;
-	}
-	public void setMobile(String mobile) {
-		this.mobile = mobile;
-	}
+	
 	
 }
