b4dfd9dd14c8a3ef478d95065b1d5c7b1413cb8e
Abhishek <https://github.com/aabhishek1990/my-work>
2017-06-20 17:31:07 +0530

    2 changes.

--- src/com/nt/model/User.java
+++ src/com/nt/model/User.java
@@ -2,6 +2,13 @@ package com.nt.model;
 
 public class User {
 
+	private int    id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
 	private String name;
 	private String address;
 	private String mobile;
